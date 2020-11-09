facb3aae6346fb4dbe3f490868f6ff8164818fdb
alex <alex.zavr.27100@gmail.com>
2020-11-09 13:17:22 +0400

    task 1

--- 7sem/task_1/t_1.py
+++ 7sem/task_1/t_1.py
@@ -1,25 +1,19 @@
-
-n = 3
 lam = [2, 5, 5]
+c = [3, 2, 1]
 mu = 15
 
 v = 1 / mu
 psy = [lam[i] / mu for i in range(len(lam))]
-
 R_n = sum(psy)
-print("R_n =", R_n)
+print("Коэффициент использования прибора любыми требованиями:", R_n)
 
 w = 0.5 * sum([2 * v ** 2 * lam[i] for i in range(len(lam))]) / (1 - R_n)
-print("w =", w)
-
-u = w + v
-print("u ", u)
+print("Средняя продолжительность ожидания в каждой очереди:", w)
+print("Среднее время ожидания произвольного заказа на обслуживание:", w)
 
 b = [lam[i] * w for i in range(len(lam))]
-print("b_i -", b)
-print("b =", sum(b))
-print("Средняя очередь", sum(b) / len(b))
+[print('Среднее число заказов в', i + 1, 'очереди:', b[i]) for i in range(len(b))]
+print('Средняя длина очереди:', sum(b))
 
-n = [b[i] + psy[i] for i in range(len(b))]
-print("n_i -", n)
-print("n =", sum(n))
+cost_f = sum([c[i] * b[i] for i in range(len(b))])
+print('Функция стоимости:', cost_f)
