47859aacce030cf744a02c3d2d45de4eac2528c3
alex <alex.zavr.27100@gmail.com>
2020-11-09 11:52:08 +0400

    sem7

--- 7sem/task_1/t_1.py
+++ 7sem/task_1/t_1.py
@@ -1,29 +1,25 @@
-def main():
-    n = 3
-    lam = [2, 5, 5]
-    mu = 15
 
-    v = 1 / mu
-    psy = [lam[i] / mu for i in range(len(lam))]
+n = 3
+lam = [2, 5, 5]
+mu = 15
 
-    R_n = sum(psy)
-    print("R_n =", R_n)
+v = 1 / mu
+psy = [lam[i] / mu for i in range(len(lam))]
 
-    w = 0.5 * sum([2 * v ** 2 * lam[i] for i in range(len(lam))]) / (1 - R_n)
-    print("w =", w)
+R_n = sum(psy)
+print("R_n =", R_n)
 
-    u = w + v
-    print("u ", u)
+w = 0.5 * sum([2 * v ** 2 * lam[i] for i in range(len(lam))]) / (1 - R_n)
+print("w =", w)
 
-    b = [lam[i] * w for i in range(len(lam))]
-    print("b_i -", b)
-    print("b =", sum(b))
-    print("Средняя очередь", sum(b) / len(b))
+u = w + v
+print("u ", u)
 
-    n = [b[i] + psy[i] for i in range(len(b))]
-    print("n_i -", n)
-    print("n =", sum(n))
+b = [lam[i] * w for i in range(len(lam))]
+print("b_i -", b)
+print("b =", sum(b))
+print("Средняя очередь", sum(b) / len(b))
 
-
-if __name__ == '__main__':
-    main()
+n = [b[i] + psy[i] for i in range(len(b))]
+print("n_i -", n)
+print("n =", sum(n))
